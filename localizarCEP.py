f569740293ddc7eeca3809a4b6e58e3743076c15
Gabriel Muniz Taking <gabriel.muniz@taking.com.br>
2021-01-28 16:06:06 -0300

    localizarCEP

--- localizarCEP.py
+++ localizarCEP.py
@@ -1,34 +1,31 @@
 import requests
-print("Olá")
+
 def main():
-
     print("---Consulta CEP---")
+    cep = input("Digite o CEP: ")
 
-   cep = input("Digite o CEP: ")
+    if len(cep) != 8:
+        print("Quantidade de dígitos inválido")
+        exit()
 
-   if len(cep) != 8:
-       print("Quantidade de dígitos inválido")
-       exit()
+    request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
 
-   request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
+    address_data = request.json()
 
-   address_data = request.json()
-
-   if 'erro' not in address_data:
-       #print(request.json())
+    if 'erro' not in address_data:
        
-       print('CEP: {}'.format(address_data['cep']))
-       print('Logradouro: {}'.format(address_data['logradouro']))
-       print('Complemento: {}'.format(address_data['complemento']))
-       print('Bairro: {}'.format(address_data['bairro']))
-       print('Cidade: {}'.format(address_data['localidade']))
-       print('Estado: {}'.format(address_data['uf']))
-   else:
-       print('{}: Cep inválido.'.format(cep))
+        print('CEP: {}'.format(address_data['cep']))
+        print('Logradouro: {}'.format(address_data['logradouro']))
+        print('Complemento: {}'.format(address_data['complemento']))
+        print('Bairro: {}'.format(address_data['bairro']))
+        print('Cidade: {}'.format(address_data['localidade']))
+        print('Estado: {}'.format(address_data['uf']))
+    else:
+        print('{}: Cep inválido.'.format(cep))
 
-   option = int(input("Nova consulta ? \n 1. Sim \n 2. Não \n"))
-   if option == 1:
-       main()
+    option = int(input("Nova consulta ? \n 1. Sim \n 2. Não \n"))
+    if option == 1:
+        main()
 
 if __name__ == "__main__":
    main()
