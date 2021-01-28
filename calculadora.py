b7f41dd7036131a1b836b0bd9da6a4681dcfdae0
raphaelmartineztaking <raphael.martinez@taking.com.br>
2021-01-28 17:24:52 -0300

    atualização calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,12 @@
 num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
 
 som = (num1+num2)
 sub = (num1-num2)
+mult = (num1*num2)
+div = (num1/num2)
 
-print(som, "\n",sub)
+print("soma : ", som)
+print("subtração: ", sub)
+print("multiplicação: ", mult)
+print("divisão: ", div)
